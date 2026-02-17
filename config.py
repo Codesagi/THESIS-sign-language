570b2d41877075366e032fca798431dd873c0158
Red <vergara.raynardethan@gmail.com>
2026-02-17 19:59:01 +0800

    v0

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-SUPABASE_URL = "https://qqzthkqjadsmlkhtabpk.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxenRoa3FqYWRzbWxraHRhYnBrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDcxOTgwNywiZXhwIjoyMDg2Mjk1ODA3fQ.bHnvUQpi-3PRDIUHem4ZHMdufykgvySTMZtX6930auU"
+SUPABASE_URL = "a"
+SUPABASE_KEY = "a"
 
 TOP_N_PREDICTIONS   = 5      # How many top matches to show on screen
 MIN_CONFIDENCE      = 0.0    # Minimum similarity % to show (0.0 = show all)
